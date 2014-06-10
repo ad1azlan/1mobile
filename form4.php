d60cf856044542e397580aa84e76458bf0c4194a
ad1azlan <precinct11@gmail.com>
2014-06-10 17:06:50 +0800

    Delete form4.php

--- form4.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST["nama"];
-$dob = $_POST["dob"];
-$jantina = $_POST["jantina"];
-
-echo "Nama... " . $name . "<br>";
-echo "DOB... " . $dob . "<br>";
-echo "Jantina... " . $jantina . "<br>";
-?>
