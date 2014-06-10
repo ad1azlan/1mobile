9f8b62fa04002493050cc13760be906e26a17364
unknown <JPAcyber@Latihan26.jpa.gov.my>
2014-06-10 15:58:06 +0800

    test

---
+++ form4.php
@@ -0,0 +1,9 @@
+<?php
+$name = $_POST["nama"];
+$dob = $_POST["dob"];
+$jantina = $_POST["jantina"];
+
+echo "Nama... " . $name . "<br>";
+echo "DOB... " . $dob . "<br>";
+echo "Jantina... " . $jantina . "<br>";
+?>
